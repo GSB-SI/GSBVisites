aeb10df9a388485b20c8618a282221e4ce675880
BarckN <nolanbarck@gmail.com>
2025-03-04 09:09:50 +0100

    Add capacitor + android

---
+++ android/app/src/main/java/fr/sio/gsbvisites/MainActivity.java
@@ -0,0 +1,5 @@
+package fr.sio.gsbvisites;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'fr.sio.gsbvisites',
   appName: 'GSBVisites',
   webDir: 'www',
 };
