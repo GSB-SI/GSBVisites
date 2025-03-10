22a5b08ce0ce0bea753b3121ecf548dae3b25b2c
BarckN <nolanbarck@gmail.com>
2025-03-10 14:44:53 +0100

    patch API

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -44,4 +44,179 @@ export class ApiService {
         })
     );
   }
+
+  getLesMedecins(args?: any) {
+    const options = {
+      url: `${this.apiUrl}/doctors`,
+      headers: {
+        Authorization: 'Bearer' + this.accountInfos.token,
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      params: args,
+    };
+    if (!args) {
+      delete options.params;
+    }
+    return from(
+      CapacitorHttp.get(options)
+        .then((res) => {
+          return res;
+        })
+        .catch((error) => {
+          console.error('Error in CapacitorHttp.post:', error);
+          throw error;
+        })
+    );
+  }
+
+  getUnMedecin(id: any) {
+    const options = {
+      url: `${this.apiUrl}/doctors/${id}`,
+      headers: {
+        Authorization: 'Bearer' + this.accountInfos.token,
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      params: id,
+    };
+    if (!id) {
+      delete options.params;
+    }
+    return from(
+      CapacitorHttp.get(options)
+        .then((res) => {
+          return res;
+        })
+        .catch((error) => {
+          console.error('Error in CapacitorHttp.post:', error);
+          throw error;
+        })
+    );
+  }
+
+  getLesRapports(args?: any) {
+    const options = {
+      url: `${this.apiUrl}/reports`,
+      headers: {
+        Authorization: 'Bearer' + this.accountInfos.token,
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      params: args,
+    };
+    if (!args) {
+      delete options.params;
+    }
+    return from(
+      CapacitorHttp.get(options)
+        .then((res) => {
+          return res;
+        })
+        .catch((error) => {
+          console.error('Error in CapacitorHttp.post:', error);
+          throw error;
+        })
+    );
+  }
+
+  getUnRapport(id: any) {
+    const options = {
+      url: `${this.apiUrl}/reports/${id}`,
+      headers: {
+        Authorization: 'Bearer' + this.accountInfos.token,
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      params: id,
+    };
+    if (!id) {
+      delete options.params;
+    }
+    return from(
+      CapacitorHttp.get(options)
+        .then((res) => {
+          return res;
+        })
+        .catch((error) => {
+          console.error('Error in CapacitorHttp.post:', error);
+          throw error;
+        })
+    );
+  }
+
+  addUnRapport(args?: any) {
+    const options = {
+      url: `${this.apiUrl}/reports`,
+      headers: {
+        Authorization: 'Bearer' + this.accountInfos.token,
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      data: args,
+    };
+    if (!args) {
+      delete options.data;
+    }
+    return from(
+      CapacitorHttp.post(options)
+        .then((res) => {
+          return res;
+        })
+        .catch((error) => {
+          console.error('Error in CapacitorHttp.post', error);
+          throw error;
+        })
+    );
+  }
+
+  updateUnRapport(id?: any) {
+    const options = {
+      url: `${this.apiUrl}/reports/${id}`,
+      headers: {
+        Authorization: 'Bearer' + this.accountInfos.token,
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      params: id,
+    };
+    if (!id) {
+      delete options.params;
+    }
+    return from(
+      CapacitorHttp.patch(options)
+        .then((res) => {
+          return res;
+        })
+        .catch((error) => {
+          console.error('Error in CapacitorHttp.patch', error);
+          throw error;
+        })
+    );
+  }
+
+  deleteUnRapport(id?: any) {
+    const options = {
+      url: `${this.apiUrl}/reports/${id}`,
+      headers: {
+        Authorization: 'Bearer' + this.accountInfos.token,
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      params: id,
+    };
+    if (!id) {
+      delete options.params;
+    }
+    return from(
+      CapacitorHttp.delete(options)
+        .then((res) => {
+          return res;
+        })
+        .catch((error) => {
+          console.error('Error in CapacitorHttp.delete', error);
+          throw error;
+        })
+    );
+  }
 }
