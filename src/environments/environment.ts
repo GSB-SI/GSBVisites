216cd3d7c0ba028aa53a42e2b366cf8c5e6aca39
Maki-SIO <maximiliengrz@gmail.com>
2025-03-19 14:13:56 +0100

    fix: Fix global

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  baseUrl: 'https://apigsb-visites.vercel.app',
+  baseUrl: 'https://gsb-visites-api.vercel.app',
   openCageApiKey: '632780c5cf6c4c00b282a271baad4db9',
 };
 
